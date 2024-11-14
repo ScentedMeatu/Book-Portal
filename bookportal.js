37117dad652ffc8718c62707e9e1adee3d265433
Bharath K H <181883405+ScentedMeatu@users.noreply.github.com>
2024-11-14 15:25:49 +0530

    create array with 10 book objects

---
+++ bookportal.js
@@ -0,0 +1,71 @@
+let books = [{
+    id: 101,
+    name: "lord of rings",
+    price: 4000,
+    status: "available",
+    quantity: 10
+},
+{
+    id: 102,
+    name: "harry potter",
+    price: 2500,
+    status: "available",
+    quantity: 10
+},
+{
+    id: 103,
+    name: "one piece",
+    price: 20000,
+    status: "available",
+    quantity: 10
+},
+{
+    id: 104,
+    name: "hobbit",
+    price: 4000,
+    status: "available",
+    quantity: 10
+},
+{
+    id: 105,
+    name: "re-zero",
+    price: 10000,
+    status: "available",
+    quantity: 10
+},
+{
+    id: 106,
+    name: "pacific rim",
+    price: 1800,
+    status: "available",
+    quantity: 10
+},
+{
+    id: 107,
+    name: "witcher",
+    price: 4000,
+    status: "available",
+    quantity: 10
+},
+{
+    id: 108,
+    name: "astrophysics for people in a Hurry",
+    price: 2000,
+    status: "available",
+    quantity: 10
+},
+{
+    id: 109,
+    name: "dairy of wimpy kid",
+    price: 1000,
+    status: "available",
+    quantity: 10
+},
+{
+    id: 110,
+    name: "game of thrones",
+    price: 5000,
+    status: "available",
+    quantity: 10
+}]
+
